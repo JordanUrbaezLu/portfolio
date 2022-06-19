afa23bc505eed9987d69e92707a900437cda5e99
Jordan Urbaez Lu <jordana.urbaez@gmail.com>
2022-06-18 21:20:48 -0400

    created courses page

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -16,46 +16,34 @@ import {
   DiScrum,
 } from "react-icons/di";
 
-import {
-    GiOctopus
-} from "react-icons/gi"
+import { GiOctopus } from "react-icons/gi";
 
 import {
-    SiCplusplus,
-    SiCodio,
-    SiTypescript,
-    SiMysql,
-    SiJest,
-    SiStorybook,
-    SiExpress,
-    SiMocha,
-    SiChai,
-    SiMaterialui,
-    SiSubversion,
-    SiAzuredevops,
-    SiWebpack,
-    SiBabel,
-    SiJson,
-    SiLodash
-} from "react-icons/si"
+  SiCplusplus,
+  SiCodio,
+  SiTypescript,
+  SiMysql,
+  SiJest,
+  SiStorybook,
+  SiExpress,
+  SiMocha,
+  SiChai,
+  SiMaterialui,
+  SiSubversion,
+  SiAzuredevops,
+  SiWebpack,
+  SiBabel,
+  SiJson,
+  SiLodash,
+} from "react-icons/si";
 
-import {
-    AiOutlineConsoleSql,
-} from "react-icons/ai"
+import { AiOutlineConsoleSql } from "react-icons/ai";
 
-import {
-    TbBrandReactNative,
-} from "react-icons/tb"
+import { TbBrandReactNative } from "react-icons/tb";
 
-import {
-    HiChartBar,
-} from "react-icons/hi"
+import { HiChartBar } from "react-icons/hi";
 
-import {
-    BiLineChartDown,
-} from "react-icons/bi"
-
-
+import { BiLineChartDown } from "react-icons/bi";
 
 const Skills = () => {
   return (
@@ -67,26 +55,25 @@ const Skills = () => {
           <div className="IconName">JavaScript</div>
         </div>
         <div className="IconContainer">
-            <SiTypescript size={50} />
-            <div className="IconName">TypeScript</div>
+          <SiTypescript size={50} />
+          <div className="IconName">TypeScript</div>
         </div>
         <div className="IconContainer">
-            <AiOutlineConsoleSql size={50} />
-            <div className="IconName">SQL</div>
+          <AiOutlineConsoleSql size={50} />
+          <div className="IconName">SQL</div>
         </div>
         <div className="IconContainer">
-            <DiPython size={50} />
-            <div className="IconName">Python</div>
+          <DiPython size={50} />
+          <div className="IconName">Python</div>
         </div>
         <div className="IconContainer">
-            <SiCplusplus size={50} />
-            <div className="IconName">C++</div>
+          <SiCplusplus size={50} />
+          <div className="IconName">C++</div>
         </div>
         <div className="IconContainer">
-            <SiCodio size={50} />
-            <div className="IconName">C</div>
+          <SiCodio size={50} />
+          <div className="IconName">C</div>
         </div>
-        
       </div>
       <div className="Skill">
         <div className="Skill-Title">Front-End Technologies: </div>
@@ -99,8 +86,8 @@ const Skills = () => {
           <div className="IconName">React Native</div>
         </div>
         <div className="IconContainer">
-            <DiHtml5 size={50} />
-            <div className="IconName">HTML5</div>
+          <DiHtml5 size={50} />
+          <div className="IconName">HTML5</div>
         </div>
         <div className="IconContainer">
           <DiCss3 size={50} />
@@ -111,78 +98,78 @@ const Skills = () => {
           <div className="IconName">Sass</div>
         </div>
         <div className="IconContainer">
-            <SiMaterialui size={50} />
-            <div className="IconName">MaterialUI</div>
+          <SiMaterialui size={50} />
+          <div className="IconName">MaterialUI</div>
         </div>
         <div className="IconContainer">
-            <SiWebpack size={50} />
-            <div className="IconName">Webpack</div>
+          <SiWebpack size={50} />
+          <div className="IconName">Webpack</div>
         </div>
         <div className="IconContainer">
-            <SiBabel size={50} />
-            <div className="IconName">Babel</div>
+          <SiBabel size={50} />
+          <div className="IconName">Babel</div>
         </div>
         <div className="IconContainer">
-            <HiChartBar size={50} />
-            <div className="IconName">Highcharts</div>
+          <HiChartBar size={50} />
+          <div className="IconName">Highcharts</div>
         </div>
         <div className="IconContainer">
-            <SiLodash size={50} />
-            <div className="IconName">Lodash</div>
+          <SiLodash size={50} />
+          <div className="IconName">Lodash</div>
         </div>
         <div className="IconContainer">
-            <DiJqueryLogo size={50} />
-            <div className="IconName">jQuery</div>
+          <DiJqueryLogo size={50} />
+          <div className="IconName">jQuery</div>
         </div>
       </div>
       <div className="Skill">
         <div className="Skill-Title">Back-End Technologies: </div>
         <div className="IconContainer">
-            <DiMongodb size={50} />
-            <div className="IconName">MongoDB</div>
+          <DiMongodb size={50} />
+          <div className="IconName">MongoDB</div>
         </div>
         <div className="IconContainer">
-            <SiMysql size={50} />
-            <div className="IconName">MySQL</div>
+          <SiMysql size={50} />
+          <div className="IconName">MySQL</div>
         </div>
         <div className="IconContainer">
-            <DiNodejs size={50} />
-            <div className="IconName">NodeJS</div>
+          <DiNodejs size={50} />
+          <div className="IconName">NodeJS</div>
         </div>
         <div className="IconContainer">
-            <SiExpress size={50} />
-            <div className="IconName">Express</div>
+          <SiExpress size={50} />
+          <div className="IconName">Express</div>
         </div>
         <div className="IconContainer">
-            <DiNpm size={50} />
-            <div className="IconName">NPM</div>
+          <DiNpm size={50} />
+          <div className="IconName">NPM</div>
         </div>
         <div className="IconContainer">
-            <SiJson size={50} />
-            <div className="IconName">JSON</div>
+          <SiJson size={50} />
+          <div className="IconName">JSON</div>
         </div>
       </div>
       <div className="Skill">
         <div className="Skill-Title">Testing: </div>
         <div className="IconContainer">
-            <SiJest size={50} />
-            <div className="IconName">Jest</div>
+          <SiJest size={50} />
+          <div className="IconName">Jest</div>
         </div>
         <div className="IconContainer">
-            <GiOctopus size={50} />
-            <div className="IconName">React Testing Library</div>
+          <GiOctopus size={50} />
+          <div className="IconName">React Testing Library</div>
         </div>
         <div className="IconContainer">
-            <SiStorybook size={50} />
-            <div className="IconName">Storybook</div>
+          <SiStorybook size={50} />
+          <div className="IconName">Storybook</div>
         </div>
         <div className="IconContainer">
-            <SiMocha size={50} />
-            <div className="IconName">Mocha</div>
+          <SiMocha size={50} />
+          <div className="IconName">Mocha</div>
         </div>
         <div className="IconContainer">
-            <SiChai size={50} />
-            <div className="IconName">Chai</div>
+          <SiChai size={50} />
+          <div className="IconName">Chai</div>
         </div>
       </div>
       <div className="Skill">
@@ -196,27 +183,27 @@ const Skills = () => {
           <div className="IconName">Github</div>
         </div>
         <div className="IconContainer">
-            <SiSubversion size={50} />
-            <div className="IconName">SVN</div>
+          <SiSubversion size={50} />
+          <div className="IconName">SVN</div>
         </div>
         <div className="IconContainer">
-            <SiAzuredevops size={50} />
-            <div className="IconName">Azure Devops</div>
+          <SiAzuredevops size={50} />
+          <div className="IconName">Azure Devops</div>
         </div>
       </div>
       <div className="Skill">
         <div className="Skill-Title">Methodologies: </div>
         <div className="IconContainer">
-            <DiJira size={50} />
-            <div className="IconName">Jira</div>
+          <DiJira size={50} />
+          <div className="IconName">Jira</div>
         </div>
         <div className="IconContainer">
-            <DiScrum size={50} />
-            <div className="IconName">Scrum</div>
+          <DiScrum size={50} />
+          <div className="IconName">Scrum</div>
         </div>
         <div className="IconContainer">
-            <BiLineChartDown size={50} />
-            <div className="IconName">Waterfall</div>
+          <BiLineChartDown size={50} />
+          <div className="IconName">Waterfall</div>
         </div>
       </div>
     </div>
